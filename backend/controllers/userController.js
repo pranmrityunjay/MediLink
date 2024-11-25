0dd3751eddbe9b9d53f407dd1a945369a3e3c1bc
Mrityunjay Kumar <pranmrityunjay@gmail.com>
2024-11-25 14:15:48 +0530

    added payment controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -5,7 +5,12 @@ import UserModel from "../models/userModel.js";
 import DoctorModel from "../models/doctorModel.js";
 import AppointmentModel from "../models/appointmentModel.js";
 import { v2 as cloudinary } from 'cloudinary'
+import razorpay from 'razorpay';
 
+const razorpayInstance = new razorpay({
+    key_id: process.env.RAZORPAY_KEY_ID,
+    key_secret: process.env.RAZORPAY_KEY_SECRET,
+})
 
 const registerUser = async (req, res) => {
     try {
@@ -168,6 +173,51 @@ const listAppointment = async (req, res) => {
     }
 }
 
+
+const paymentRazorpay = async (req, res) => {
+    try {
+
+        const { appointmentId } = req.body
+        const appointmentData = await AppointmentModel.findById(appointmentId)
+
+        if (!appointmentData || appointmentData.cancelled) {
+            return res.json({ success: false, message: 'Appointment Cancelled or not found' })
+        }
+
+        const options = {
+            amount: appointmentData.amount * 100,
+            currency: process.env.CURRENCY,
+            receipt: appointmentId,
+        }
+
+        const order = await razorpayInstance.orders.create(options)
+
+        res.json({ success: true, order })
+
+    } catch (error) {
+        console.log(error)
+        res.json({ success: false, message: error.message })
+    }
+}
+
+const verifyRazorpay = async (req, res) => {
+    try {
+        const { razorpay_order_id } = req.body
+        const orderInfo = await razorpayInstance.orders.fetch(razorpay_order_id)
+
+        if (orderInfo.status === 'paid') {
+            await AppointmentModel.findByIdAndUpdate(orderInfo.receipt, { payment: true })
+            res.json({ success: true, message: "Payment Successful" })
+        }
+        else {
+            res.json({ success: false, message: 'Payment Failed' })
+        }
+    } catch (error) {
+        console.log(error)
+        res.json({ success: false, message: error.message })
+    }
+}
+
 export {
     loginUser,
     registerUser,
@@ -176,4 +226,6 @@ export {
     bookAppointment,
     listAppointment,
     cancelAppointment,
+    paymentRazorpay,
+    verifyRazorpay
 }
